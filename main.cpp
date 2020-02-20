c87425cd4b40ca9f7f7532cb689d723719eea66f
greenTransistor <artur.viligurskyi@gmail.com>
2020-02-20 19:24:22 +0200

    added main.cpp draft

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	//
+
+	return 0;
+}
