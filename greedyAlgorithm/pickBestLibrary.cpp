f35c93aaba30c9e78506f9d06d6e0e2b72e83def
scientist19 <40214489+scientist19@users.noreply.github.com>
2020-02-20 22:13:31 +0200

    fixed bugs

--- greedyAlgorithm/pickBestLibrary.cpp
+++ greedyAlgorithm/pickBestLibrary.cpp
@@ -4,13 +4,13 @@ std::pair<Library*, std::vector<Book*> > pickBestLibrary(long long daysRemained)
 	std::vector<Book*> bestLibBooks;
 	long long bestScore = 0;
 
-	for (auto library : libraries){
+	for (auto& library : libraries){
 		if (library.isSigned || library.daysToSignIn >= daysRemained) continue;
 		long long score = 0;
 		std::vector<Book*> books;
 
 		long long cnt = 0;
-		for (long long i = 0; i < library.books.size() && cnt < daysRemained*library.booksPerDay; i++){
+		for (long long i = 0; i < library.books.size() && cnt < (daysRemained-library.daysToSignIn)*library.booksPerDay; i++){
 			auto book = library.books[i];
 			if (book->isScanned) continue;
 
