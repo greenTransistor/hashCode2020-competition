c73f9bdeb6db1bbd825969d84db7ea7bc38e4937
scientist19 <40214489+scientist19@users.noreply.github.com>
2020-02-20 21:19:41 +0200

    added method pickBestLibrary

--- greedyAlgorithm/pickBestLibrary.cpp
+++ greedyAlgorithm/pickBestLibrary.cpp
@@ -1,4 +1,30 @@
 
-Library* pickBestLibrary(long long daysRemained){
-	return nullptr;
+std::pair<Library*, std::vector<Book*> > pickBestLibrary(long long daysRemained){
+	Library* bestLib = nullptr;
+	std::vector<Book*> bestLibBooks;
+	long long bestScore = 0;
+
+	for (auto library : libraries){
+		if (library.isSigned) continue;
+		long long score = 0;
+		std::vector<Book*> books;
+
+		long long cnt = 0;
+		for (long long i = 0; i < library.books.size() && cnt < daysRemained*library.booksPerDay; i++){
+			auto book = library.books[i];
+			if (book->isSigned) continue;
+
+			score += book->score;
+			books.push_back(book);
+			cnt++;
+		}
+
+		if (score > bestScore){
+			bestScore = score;
+			bestLib = &library;
+			bestLibBooks = books;
+		}
+	}
+
+	return {bestLib, bestLibBooks};
 }
