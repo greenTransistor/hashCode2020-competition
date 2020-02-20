ac1ac90af92736f494bffded6e78c6a4c57073d5
greenTransistor <artur.viligurskyi@gmail.com>
2020-02-20 21:11:04 +0200

    added data reading and greedy algorithm (not implemented yet) calling

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 struct Book {
 	long long index;
-	bool isScanned;
+	bool isScanned; // Mutable
 	long long score;
 };
 
@@ -25,7 +25,7 @@ struct Library {
 	long long booksPerDay;
 	long long daysToSignIn;
 	long long index;
-	bool isSigned;
+	bool isSigned; // Mutable
 };
 
 struct Answer {
@@ -39,13 +39,20 @@ vector<Book> books;
 vector<Library> libraries;
 long long totalDays;
 
-long long bestScore = 0;
+long long bestScore;
+Answer bestAnswer;
 
 // }
+
+#include "greedyAlgorithm/greedyAlgorithm.cpp"
 
 const int FILES_COUNT = 6;
 const string FILE_NAMES[FILES_COUNT] = {"a_example", "b_read_on", "c_incunabula", "d_tough_choices", "e_so_many_books", "f_libraries_of_the_world"};
 string fileName, inputFileName, outputFileName;
+
+bool booksComparator(Book* book1, Book* book2) {
+	return (book1->score < book2->score);
+}
 
 void init() {
 	for (size_t i = 0; i < books.size(); i++) {
@@ -59,10 +66,13 @@ void init() {
 void readInputFile(string name) {
 	ifstream inf(name);
 	long long booksCount;
+	long long booksCountInCurrentLibrary;
 	long long librariesCount;
+	long long bookIndex;
 	Book newBook;
 	Library newLibrary;
-	size_t i;
+	long long i;
+	long long j;
 
 	inf >> booksCount >> librariesCount >> totalDays;
 	books.resize(booksCount);
@@ -70,16 +80,26 @@ void readInputFile(string name) {
 	for (i = 0; i < booksCount; i++) {
 		newBook = Book();
 		inf >> newBook.score;
+		books[i] = newBook;
 	}
 	for (i = 0; i < librariesCount; i++) {
 		newLibrary = Library();
+		inf >> booksCountInCurrentLibrary;
+		newLibrary.books.resize(booksCountInCurrentLibrary);
+		inf >> newLibrary.daysToSignIn >> newLibrary.booksPerDay;
+		for (j = 0; j < booksCountInCurrentLibrary; j++) {
+			inf >> bookIndex;
+			newLibrary.books[j] = &books[bookIndex];
+		}
+		sort(newLibrary.books.begin(), newLibrary.books.end(), booksComparator);
+		libraries[i] = newLibrary;
 	}
 
 	inf.close();
 }
 
 void writeOutputFile(string name) {
-	//
+	// bestAnswer
 }
 
 int main() {
@@ -91,7 +111,7 @@ int main() {
 
 		bestScore = 0;
 		init();
-		//algorithm1();
+		greedyAlgorithm();
 
 		//init();
 		//algorithm2();
