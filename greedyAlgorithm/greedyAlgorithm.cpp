208a43002f929ee012598a1c0a5f0d387947e1c6
AndLvovSky <viktorilvovskyi@gmail.com>
2020-02-20 21:52:07 +0200

    Add greedy algorithm

--- greedyAlgorithm/greedyAlgorithm.cpp
+++ greedyAlgorithm/greedyAlgorithm.cpp
@@ -1,4 +1,32 @@
+#include "pickBestLibrary.cpp"
 
-void greedyAlgorithm(){
+void addBestLibrary(Answer& answer, long long& daysRemaining, bool& allLibrariesSigned) {
+	pair<Library*, vector<Book*>> bestLibraryAndBooks = pickBestLibrary(daysRemaining);
+	Library* bestLibrary = bestLibraryAndBooks.first;
 
+	if (bestLibrary == nullptr) {
+		allLibrariesSigned = true;
+		return;
+	}
+
+	vector<Book*> books = bestLibraryAndBooks.second;
+
+	bestLibrary->isSigned = true;
+	for (Book* book: books) {
+		book->isScanned = true;
+	}
+
+	answer.libraries.push_back(bestLibrary);
+	answer.booksPerLibrary.push_back(books);
+
+	daysRemaining -= bestLibrary->daysToSignIn;
+}
+
+void greedyAlgorithm() {
+	Answer answer;
+	long long daysRemaining = totalDays;
+	bool allLibrariesSigned = false;
+	while (daysRemaining && !allLibrariesSigned) {
+		addBestLibrary(answer, daysRemaining, allLibrariesSigned);
+	}
 }
